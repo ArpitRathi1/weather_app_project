242cf85d786ee6bb831af6dca405c34af6df73e3
ArpitRathi1 <rathi844@gmail.com>
2024-08-02 13:27:21 +0530

    update script

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ const loadingScreen=document.querySelector(".loading-container");
 const userInfoContainer=document.querySelector(".user-info-container");
 
 
+let currentTab=userTab;//by default
+const API_KEY="e03935d1a4968e4047097d2db1b7b47e";
+currentTab.classList.add("current-tab");
+getfromSessionStorage()
